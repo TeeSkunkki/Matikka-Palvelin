61a362850f54170550f916dded4e92b25ef0ac97
TeeSkunkki <70936809+TeeSkunkki@users.noreply.github.com>
2022-05-23 14:19:13 +0300

    turhat tiedostot poistettu

--- Server.js
+++ Server.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-var kuvat = ["1","2","3","4","5","6","7","8","9","0"];
 
 app.get("/Kuvat", function(req, res){
     console.log(req.route);
@@ -15,12 +14,6 @@ app.get('/Kuvat/*/',function(req, res){
     console.log(asda[0] + " " + asda[1] + " Kaiffarointi 1");
     res.send(fs.readdirSync('./Public/KaikkiKuvat/' + asda[0] + '/' + asda[1]));
 });
-//app.get('/Kuvat/*/?*',function(req, res){
-/*    var asda = req.url;
-    asda = asda.split('/').splice(2,2);
-    console.log(asda + " Kaiffarointi 2");
-    res.send(fs.readdirSync('./Public/KaikkiKuvat/' + asda + '/'));
-});*/
 
 app.use(express.static("Public"));
 
